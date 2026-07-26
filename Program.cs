ca26208d204a4a575161152aa7b33847bcf2682a
lispenmerlinraja-maker <lispenmerlinraja@gmail.com>
2026-07-26 17:21:03 +0000

    Added Thread Program

--- Program.cs
+++ Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
+using System.Threading;
+
+class ThreadDemo
+{
+    public void DisplayNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Number Thread : " + i);
+            Thread.Sleep(500);
+        }
+    }
+
+    public void DisplayAlphabets()
+    {
+        for (char ch = 'A'; ch <= 'E'; ch++)
+        {
+            Console.WriteLine("Alphabet Thread : " + ch);
+            Thread.Sleep(500);
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
+        ThreadDemo obj = new ThreadDemo();
 
-        if (number % 2 == 0)
-            Console.WriteLine(number + " is Even");
-        else
-            Console.WriteLine(number + " is Odd");
+        Thread t1 = new Thread(obj.DisplayNumbers);
+        Thread t2 = new Thread(obj.DisplayAlphabets);
+
+        Console.WriteLine("Starting Threads...\n");
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine("\nBoth Threads Completed.");
+
+        Console.ReadKey();
     }
 }
