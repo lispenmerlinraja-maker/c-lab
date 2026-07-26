5887dcd9c26ad4279f6dea34205b4b92f8fa9acb
lispenmerlinraja-maker <lispenmerlinraja@gmail.com>
2026-07-26 21:46:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+            Console.WriteLine(number + " is Even");
+        else
+            Console.WriteLine(number + " is Odd");
+    }
+}
